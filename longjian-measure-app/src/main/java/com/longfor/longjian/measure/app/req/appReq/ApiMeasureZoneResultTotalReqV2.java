06301baf873935863d80d22a8ebd8bd6c71961a7
wangxueshuo <wangxueshuo@longfor.com>
2018-12-14 16:46:04 +0800

    http://192.168.37.159:3000/project/8/interface/api/434 http://192.168.37.159:3000/project/8/interface/api/580 接口开发完成

---
+++ longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/req/appReq/ApiMeasureZoneResultTotalReqV2.java
@@ -0,0 +1,19 @@
+package com.longfor.longjian.measure.app.req.appReq;
+
+import lombok.Data;
+
+@Data
+public class ApiMeasureZoneResultTotalReqV2 {
+    /**
+     * 语言 取值范围及含义：;1 - 简体中文; ;2 - English;
+     */
+    private Integer lang;
+    /**
+     * 清单ID
+     */
+    private Integer list_id;
+    /**
+     * 上次更新时间
+     */
+    private Integer timestamp;
+}
