c4ecddd9231027090ca71114e0aa407fba5ee760
wangxueshuo <wangxueshuo@longfor.com>
2018-12-17 09:57:02 +0800

    丢失代码

--- longjian-measure-domain/src/main/java/com/longfor/longjian/measure/dao/zhijian2/MeasureSquadUserMapper.java
+++ longjian-measure-domain/src/main/java/com/longfor/longjian/measure/dao/zhijian2/MeasureSquadUserMapper.java
@@ -2,6 +2,17 @@ package com.longfor.longjian.measure.dao.zhijian2;
 
 import com.longfor.gaia.gfs.data.mybatis.LFMySQLMapper;
 import com.longfor.longjian.measure.po.zhijian2.MeasureSquadUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MeasureSquadUserMapper extends LFMySQLMapper<MeasureSquadUser> {
+    /**
+     *
+     * @param projectId
+     * @param list_id
+     * @param updateAtGt
+     * @return
+     */
+    List<MeasureSquadUser> searchMeasureSquadUserByListIdTimestampGt(@Param("projectId") Integer projectId, @Param("listId") Integer list_id, @Param("updateAtGt") String updateAtGt);
 }
