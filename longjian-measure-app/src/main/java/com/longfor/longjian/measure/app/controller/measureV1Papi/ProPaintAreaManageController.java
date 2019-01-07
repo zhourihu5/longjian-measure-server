f84aa2c0755a9ecfd3c163a34928b62e29619d7e
wangxueshuo <wangxueshuo@longfor.com>
2019-01-07 16:12:22 +0800

    commit

--- longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/controller/measureV1Papi/ProPaintAreaManageController.java
+++ longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/controller/measureV1Papi/ProPaintAreaManageController.java
@@ -1,4 +1,4 @@
-package com.longfor.longjian.measure.app.controller.paintAreaController;
+package com.longfor.longjian.measure.app.controller.measureV1Papi;
 
 import com.longfor.longjian.common.base.LjBaseResponse;
 import com.longfor.longjian.measure.app.appService.paintAreaService.IProPaintAreaManageService;
