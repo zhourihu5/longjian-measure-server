ca03272a78419a5d1683a5a3f8e6e89182f0712e
wangxueshuo <wangxueshuo@longfor.com>
2019-02-18 15:52:37 +0800

    bug

---
+++ longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/vo/staffVo/RepairerListSearchInfoVo.java
@@ -0,0 +1,10 @@
+package com.longfor.longjian.measure.app.vo.staffVo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RepairerListSearchInfoVo {
+    List<RepairerListSearchVo> repairer_info;
+}
