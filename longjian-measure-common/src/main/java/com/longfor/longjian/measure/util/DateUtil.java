faea624cc735f30a31dfd9ac0f4ea1039ed3be26
wangxueshuo <wangxueshuo@longfor.com>
2019-03-05 17:17:32 +0800

    sonar

--- longjian-measure-common/src/main/java/com/longfor/longjian/measure/util/DateUtil.java
+++ longjian-measure-common/src/main/java/com/longfor/longjian/measure/util/DateUtil.java
@@ -23,12 +23,6 @@ public class DateUtil {
 
     private static final Logger logger = LoggerFactory.getLogger(DateUtil.class);
     // 取得本地时间：
-    private static final Calendar cal = Calendar.getInstance();
-    // 取得时间偏移量：
-    private static final int zoneOffset = cal.get(java.util.Calendar.ZONE_OFFSET);
-    // 取得夏令时差：
-    private static final int dstOffset = cal.get(java.util.Calendar.DST_OFFSET);
-
 
     //日期格式，精确到日 2017-4-16
     public static final DateFormat formatter = DateFormat.getDateInstance();
@@ -257,8 +251,4 @@ public class DateUtil {
         return zoneTime.withFixedOffsetZone().format(formatter);
     }
 
-
-//    public static void main(String[] args) {
-//        System.out.println(getZoneDateTime(new Date()));
-//    }
 }
