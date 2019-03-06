6c3df98ad65293d9c580688c3b27911632e71eb9
wangxueshuo <wangxueshuo@longfor.com>
2019-03-06 11:19:44 +0800

    TEST

--- longjian-measure-app/src/test/java/com/longfor/longjian/measure/web/MeasureListSearchTest.java
+++ longjian-measure-app/src/test/java/com/longfor/longjian/measure/web/MeasureListSearchTest.java
@@ -1,9 +1,8 @@
 package com.longfor.longjian.measure.web;
 
 
-import com.alibaba.fastjson.JSON;
+import com.longfor.longjian.common.filter.UrlFilter;
 import com.longfor.longjian.measure.Application;
-import com.longfor.longjian.measure.app.req.measurelist.ConditionSearchReq;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -28,40 +26,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 
 @RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！
-@SpringBootTest(classes = Application.class) // 指定我们SpringBoot工程的Application启动类
-@WebAppConfiguration
+@SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) // 指定我们SpringBoot工程的Application启动类
 public class MeasureListSearchTest {
-
+    private static final String TOKEN = "WK72kB2Q-cOM3hpjijNydMuQDhi3cd_sqEnIrKuh0p4r_cjiISVrK99gom-hPmf7";
     private MockMvc mockMvc;
-
     @Autowired
     protected WebApplicationContext wac;
-
     @Before()
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).addFilter(new UrlFilter()).build();
     }
 
-
-//    @Test
-//    public void testConditionSearch() throws Exception {
-//        ConditionSearchReq conditionSearchReq = new ConditionSearchReq();
-//        conditionSearchReq.setGroup_id(4);
-//        conditionSearchReq.setArea_id("");
-//        conditionSearchReq.setCategory_key("");
-//        conditionSearchReq.setFinish_status(null);
-//        conditionSearchReq.setName("");
-//        conditionSearchReq.setPage(1);
-//        conditionSearchReq.setPage_size(20);
-//        conditionSearchReq.setProject_id(927);
-//        conditionSearchReq.setUser_id_list("");
-//        mockMvc.perform(
-//                post("measure/v1/papi/measure_list/condition_search")
-//                        .contentType(MediaType.APPLICATION_JSON_VALUE)
-//                        .content(JSON.toJSONString(conditionSearchReq)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.message").value("success"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andReturn();
-//    }
+    @Test
+    public void testConditionSearch() throws Exception {
+        mockMvc.perform(
+                post("/measure/v1/papi/measure_list/condition_search").header("token",TOKEN)
+                        .param("group_id","4")
+                        .param("area_id","")
+                        .param("category_key","")
+                        .param("finish_status","")
+                        .param("name","")
+                        .param("page","1")
+                        .param("page_size","20")
+                        .param("project_id","927")
+                        .param("user_id_list","")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("success"))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
 }
