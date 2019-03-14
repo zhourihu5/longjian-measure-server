ceb4c793c7ca3d5de1c86af09a56d73ee24d40b3
wangxueshuo <wangxueshuo@longfor.com>
2019-03-14 15:38:09 +0800

    sonar test

--- longjian-measure-app/src/test/java/com/longfor/longjian/measure/web/MeasureListIssueTest.java
+++ longjian-measure-app/src/test/java/com/longfor/longjian/measure/web/MeasureListIssueTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！
 @SpringBootTest(classes = Application.class,webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) // 指定我们SpringBoot工程的Application启动类
 public class MeasureListIssueTest {
-    private static final String TOKEN = "dDpocEnoBSCRXUrE-60vIroGFtdtV_TqzI-KMrv14pC3uyrUSBdyf3IdSGDf4K5K";
+    private static final String TOKEN = "IttoRbVgCglLi6cYI07SqvRrZUXinj2lQJpZ-ZXY2NdIzGCGGwxHIBhMtE5ltg9e";
     private MockMvc mockMvc;
     @Autowired
     protected WebApplicationContext wac;
