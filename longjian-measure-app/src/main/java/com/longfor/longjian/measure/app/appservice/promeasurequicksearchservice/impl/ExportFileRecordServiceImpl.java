08a19848b832e52bc1b24ca6dff48f85de7cebdf
a-jiazhongmin <a-jiazhongmin>
2019-03-22 10:41:41 +0800

    修改BUG

--- longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/appservice/promeasurequicksearchservice/impl/ExportFileRecordServiceImpl.java
+++ longjian-measure-app/src/main/java/com/longfor/longjian/measure/app/appservice/promeasurequicksearchservice/impl/ExportFileRecordServiceImpl.java
@@ -46,7 +46,7 @@ public class ExportFileRecordServiceImpl implements IExportFileRecordService {
             String baseDir = exportVo.getMeasure_base_dir();
             Integer ts = DateUtil.dateToTimestamp(new Date());
             inputFilename = String.format("%d%d.%s", randCount, ts, "input");
-            outputFilename = String.format("/export/%d%d.%s", randCount, ts, "output");
+            outputFilename = String.format("export/%d%d.%s", randCount, ts, "output");
             String filepath = String.format("%s/%s", baseDir, inputFilename);
             log.info("{}",filepath);
             this.writeInput(data, exportName, filepath);
@@ -63,7 +63,7 @@ public class ExportFileRecordServiceImpl implements IExportFileRecordService {
         insertFullVo.setParams(String.format("%s %s", inputFilename, outputFilename));
         insertFullVo.setResultFilePath(filepath);
         insertFullVo.setResultName(exportName);
-        insertFullVo.setStatus(0);
+        insertFullVo.setStatus(10);
         insertFullVo.setErrorMsg("");
         insertFullVo.setExecuteAt(executeAt);
         Map<String, Object> map = exportFileRecordDomainService.insertFull(insertFullVo);
